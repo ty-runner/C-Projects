5c3ec989ded0d5f32e2454ddf7ede186602856d1
ty-runner <ty.runner02@gmail.com>
2023-07-25 15:28:26 -0700

    Char to int

--- Algorithms/BubbleSort.c
+++ Algorithms/BubbleSort.c
@@ -5,19 +5,25 @@
 
 int main(int argc, char const *argv[])
 {
-    /* code */
-    FILE* fp = fopen("argv[1]", "r");
+    FILE* fp = fopen(argv[1], "r");
     if(fp == NULL){
         return 1;
     }
-    char text[250];
-    printf("gets tehre\n");
-    while(fgets(text, sizeof(text), fp))
+    int digit;
+    do
     {
-        
-    }
-    printf("%s\n", text);
+        //*text += fgetc(fp);
+        digit = fgetc(fp);
+        if(feof(fp)){
+            break;
+        }
+        if(digit != 32 || digit != -1){
+            //printf("%c\n", digit);
+        }
+    }while(1);
+    printf("%c\n", digit);
     return 0;
 }
 
-//vscode wont compile?
+//gcc compile
+//./a TEXTFILE
