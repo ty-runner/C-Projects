894a08fd022619facb7a76111decd1822a93afb8
ty-runner <ty.runner02@gmail.com>
2023-07-26 21:42:44 -0700

    Added swap function

--- Algorithms/BubbleSort.c
+++ Algorithms/BubbleSort.c
@@ -3,6 +3,19 @@
 #include <string.h>
 #include <stdbool.h>
 
+
+
+int* bubblesort(int* arr,int size){
+    for(int i = 0; i < size; i++){
+
+    }
+}
+void swap(int* arr, int first){
+    int temp = arr[first];
+    arr[first] = arr[first+1];
+    arr[first+1] = temp;
+}
+
 int main(int argc, char const *argv[])
 {
     FILE* fp = fopen(argv[1], "r");
@@ -10,18 +23,23 @@ int main(int argc, char const *argv[])
         return 1;
     }
     int digit;
+    int i = 0;
+    int arr[5];
     do
     {
-        //*text += fgetc(fp);
         digit = fgetc(fp);
         if(feof(fp)){
             break;
         }
-        if(digit != 32 || digit != -1){
-            //printf("%c\n", digit);
+        if(digit != 32){
+            arr[i] = digit-48;
+            i++;
         }
     }while(1);
-    printf("%c\n", digit);
+    swap(arr, 2); //tested swapping
+    for(int j = 0; j < 5; j++){
+        printf("%d\n", arr[j]);
+    }
     return 0;
 }
 
